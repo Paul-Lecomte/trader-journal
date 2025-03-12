8c95f3712ef23b3cb2fcb645b4291b2406513624
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-12 10:10:06 +0100

    reg the chart

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,2 +1,14 @@
 import { Line, Bar, Scatter } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from "chart.js"
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from "chart.js";
+
+// Register the chart components
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement
+);
