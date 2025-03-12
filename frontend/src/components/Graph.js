591f1377ead91b4228404d6b2159a82ebce0c7d0
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-12 10:09:20 +0100

    import

---
+++ frontend/src/components/Graph.js
@@ -0,0 +1,2 @@
+import { Line, Bar, Scatter } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement } from "chart.js"
