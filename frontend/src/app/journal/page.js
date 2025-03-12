94e167c65e225c614c0ff7234832a2cfe0f559d8
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-12 07:41:58 +0100

    added the imports

---
+++ frontend/src/app/journal/page.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+import { TradeTable } from "../../components/TradeTable";
+import { Graph } from "../../components/Graph";
+import { CommentsSection } from "../../components/CommentsSection";
+import { fetchData } from "../utils/storage";
