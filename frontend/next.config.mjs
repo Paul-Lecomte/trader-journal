40f0753a3d3a954ca403a56874825959d6168b5a
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-13 08:21:17 +0100

    fucking around

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export", // Static export configuration
+    images: {
+        unoptimized: true, // Disable image optimization for static export
+    },
+    distDir: "frontend_build", // Output to frontend_build directory
+};
 
 export default nextConfig;
