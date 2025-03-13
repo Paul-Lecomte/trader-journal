e1b9331b798b73e9433c67660e773a15dfba72ee
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-13 07:34:48 +0100

    storage utils

---
+++ frontend/src/app/utils/storage.js
@@ -0,0 +1,16 @@
+export const fetchData = async () => {
+    return new Promise((resolve) => {
+        chrome.storage.local.get("trades", (data) => {
+            resolve(data.trades || []);
+        });
+    });
+};
+
+export const saveComment = async (tradeId, comment) => {
+    chrome.storage.local.get("trades", (data) => {
+        const updatedTrades = data.trades.map((trade) =>
+            trade.id === tradeId ? { ...trade, comments: comment } : trade
+        );
+        chrome.storage.local.set({ trades: updatedTrades });
+    });
+};
